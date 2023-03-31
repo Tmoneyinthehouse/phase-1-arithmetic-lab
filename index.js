2ebb952f5b28abe474a06ad4f1d0070f247cf43f
Tyler Brown <tylerbrown487@gmail.com>
2023-03-30 22:23:57 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+const num1=2;
+const num2=31;
 
-// Write your code here
+const multiply=Number(num1)*Number(num2);
+
+
+const random=parseInt(Math.random(2, 1000)*100)
+
+const num3=28;
+const num4=6;
+const mod=Number(num3)%Number(num4);
+
+let sets=[0, 20, 10, 12, 19];
+let max=Math.max(...sets);
